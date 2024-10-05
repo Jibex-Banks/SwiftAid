44cc30babaedddfab96bdd760d978893ab9bc0cd
Jibex-Banks <mudathirajibolamudathir@gmail.com>
2024-10-05 23:41:51 +0100

    5

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   root:'.',
-  publicDir:'/',
+  publicDir:'src',
   base:'/SwiftAid/',
   plugins: [react()],
   resolve: {
